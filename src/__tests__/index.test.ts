8e027cafcb2bfe99332ced0c4c6b8aa62858322a
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-06-16 17:27:49 +0200

    adding real sources for odoo xmlrpc client

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,37 @@
 import { Odoo } from '../index';
 
-test('My Odoo', () => {
-    expect(new Odoo()).toBeDefined();
+test('My Odoo', async () => {
+  const odoo = Odoo({
+    url: 'http://localhost',
+    port: 8069,
+    db: 'odoo',
+    username: 'admin',
+    password: 'admin',
+  });
+
+  try {
+    await odoo.executeKw('', '');
+    expect(true).toBe(false); // should always throw an exception
+  } catch (error) {
+    console.log(error);
+    expect(error).toBeDefined();
+  }
+
+  const uid = await odoo.authenticate();
+  expect(uid).toBe(1);
+
+  const ids = await odoo.executeKw(
+    'res.partner',
+    'search',
+    [[['customer', '=', true]]],
+    {},
+  );
+  expect(ids).toEqual([7]);
+
+  const res = await odoo.executeKw('res.partner', 'read', ids, {
+    fields: ['name', 'country_id', 'comment', 'is_company', 'customer'],
+  });
+  expect(res).toHaveLength(1);
+  expect(res[0].id).toBe(7);
+  expect(res[0].name).toBe('Cliente 01');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,83 @@
-export class Odoo {
+import { Client, createClient, createSecureClient } from 'xmlrpc';
+import { parse as urlParse } from 'url';
+
+type OdooConfig = {
+  url: string;
+  host?: string;
+  port?: number;
+  db?: string;
+  username?: string;
+  password?: string;
+  secure?: boolean;
 };
+
+export function Odoo(config: OdooConfig) {
+  const urlparts = urlParse(config.url);
+  config.host = urlparts.hostname || undefined;
+  if (urlparts.port) {
+    config.port = parseInt(urlparts.port, 10);
+  }
+  config.secure = urlparts.protocol === 'https:';
+  let uid = 0;
+
+  /**
+   * Create and returns an xmlrpc client
+   * @param {string} path path to use for the connection
+   * @returns {Client} xmlrpc client
+   */
+  const getClient = (path: string) => {
+    const createClientFn = config.secure ? createSecureClient : createClient;
+    return createClientFn({
+      host: config.host,
+      port: config.port,
+      path,
+    });
+  };
+
+  const methodCall = (client: Client, method: string, params: any[]): any => {
+    return new Promise((resolve, reject) => {
+      client.methodCall(method, params, (err, value) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(value);
+      });
+    });
+  };
+
+  const authenticate = async (): Promise<number> => {
+    try {
+      uid = (await methodCall(getClient('/xmlrpc/2/common'), 'authenticate', [
+        config.db,
+        config.username,
+        config.password,
+        {},
+      ])) as number;
+      return uid;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const executeKw = (
+    model: string,
+    method: string,
+    args: any[] = [],
+    kwargs: object = {},
+  ): Promise<any> => {
+    if (!uid) {
+      throw new Error('User is not logged');
+    }
+    return methodCall(getClient('/xmlrpc/2/object'), 'execute_kw', [
+      config.db,
+      uid,
+      config.password,
+      model,
+      method,
+      args,
+      kwargs,
+    ]);
+  };
+
+  return { authenticate, executeKw };
+}
